65bf6a4050aba5a0f839bb40ebdcd20b3604e3b7
abederrahmane <abderrahmaneabed.info@gmail.com>
2024-12-08 17:16:22 +0100

    i add a frontend project

---
+++ frontend/app/component/auth.jsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello world
+        </Text>
+        <Text style={styles.subtitle}>welcome to checkin</Text>
+      </View>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
