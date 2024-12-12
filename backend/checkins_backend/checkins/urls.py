5515c0ddacba71d4c644cd9e82f070081f394f9f
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2024-12-12 02:35:31 +0100

    minor updates and changes

--- backend/checkins_backend/checkins/urls.py
+++ backend/checkins_backend/checkins/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('qr-code-scan/', views.ScanQRCodeView.as_view(), name="scan_qr_code"),
+    path('', views.ScanQRCodeView.as_view(), name="scan_qr_code"),
 ]
