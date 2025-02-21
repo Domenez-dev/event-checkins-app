f7a2b151835d06074587462b3589122aedd6555b
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-02-21 13:40:38 +0100

    updated settings

--- backend/checkins_backend/checkins_backend/settings.py
+++ backend/checkins_backend/checkins_backend/settings.py
@@ -29,7 +29,6 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 # For dev Only
 DEBUG = os.environ["DEBUG"] == 'TRUE'
